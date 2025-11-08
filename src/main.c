cacd4bf01af14ac99b767355f2d7e082d0200948
xmm16 <eshaan2031@icloud.com>
2025-11-07 23:06:41 -0600

    Updated the data structures and initialized some of them

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 enum token_type;
 char** symbols;
@@ -8,7 +9,7 @@ typedef struct token_struct token;
 enum node_type;
 typedef struct node_struct node;
 
-int main(){
+int main(int argc, char** argv){
 	char* symbols[] = {"+=", "-="};
 
 	enum token_type {
@@ -27,4 +28,22 @@ int main(){
 		token* token_argument;
 	};
 
+	struct node_struct {
+		enum node_type type;
+		token* token_argument;
+		node* back;
+		node* left;
+		node* right;
+	};
+
+	token code_lex[strlen(argv[1])]; // "argv[1]" because I don't want to have to deal with file management until I need to
+	for (int i = 0; i < strlen(argv[1]); i++){
+		
+	}
+	
+	node* code_tree;
+	code_tree->type = PROGRAM;
+	code_tree->back = NULL;
+	code_tree->left = malloc(sizeof(node));
+	code_tree->right = malloc(sizeof(node));
 }
