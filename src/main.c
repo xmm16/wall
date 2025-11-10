4e981e31fb4b7d2445d1bf86432336c301aa6f3d
xmm16 <eshaan2031@icloud.com>
2025-11-10 16:14:23 +0000

    HELLO

--- src/main.c
+++ src/main.c
@@ -9,6 +9,37 @@ struct token_struct;
 enum node_type;
 struct node_struct;
 
+int check_different_sized_strings(char* original_string_literal, char* check_string_literal){
+	size_t strlen_check_string = strlen(check_string_literal);
+    size_t strlen_original_string = strlen(original_string_literal);
+
+    char original_string[strlen_original_string];
+    strcpy(original_string, original_string_literal);
+
+    char check_string[strlen_check_string];
+    strcpy(check_string, check_string_literal);
+
+    if (strlen_original_string > strlen_check_string){
+        char replaced_char = original_string[strlen_check_string];
+        original_string[strlen_check_string] = '\0';
+
+        int return_value = strcmp(original_string, check_string);
+        original_string[strlen_check_string] = replaced_char;
+
+        return return_value;
+    } else if (strlen_check_string > strlen_original_string){
+        char replaced_char = check_string[strlen_original_string];
+        check_string[strlen_original_string] = '\0';
+
+        int return_value = strcmp(original_string, check_string);
+        check_string[strlen_original_string] = replaced_char;
+
+        return return_value;
+    } else {
+        return strcmp(original_string, check_string);
+    }
+}
+
 int main(int argc, char** argv){
 	char* symbols[] = {"+=", "-="};
 
@@ -36,8 +67,8 @@ int main(int argc, char** argv){
 		struct node_struct* right;
 	} node;
 
-	size_t strlen_argv_1 = strlen(argv[1]);
-	token code_lex[strlen_argv_1]; // "argv[1]" because I don't want to have to deal with file management until I need to
+	size_t strlen_argv_1 = strlen(argv[1]); // "argv[1]" because I don't want to have to deal with file management until I need to
+	token code_lex[strlen_argv_1];
 	for (int i = 0; i < strlen_argv_1; i++){
 		
 	}
@@ -47,4 +78,5 @@ int main(int argc, char** argv){
 	code_tree.back = NULL;
 	code_tree.left = malloc(sizeof(node));
 	code_tree.right = malloc(sizeof(node));
+
 }
