a1a69ed63d7c01b45374b2cac08fa0a91e013915
xmm16 <eshaan2031@icloud.com>
2025-11-14 16:01:32 +0000

    unary +/- symbols

--- src/main.c
+++ src/main.c
@@ -935,6 +935,7 @@ void tree(node *code_tree_ptr, token *code_lex, size_t code_lex_index) {
     switch (id) {
     case '-':
     case '+':;
+
       int restore_i = i;
       i--;
       while (i != -1 && code_lex[i].type != '+' && code_lex[i].type != '-') {
@@ -1062,6 +1063,35 @@ void tree(node *code_tree_ptr, token *code_lex, size_t code_lex_index) {
       id = code_lex[i].type;
 
     switch (id) {
+      // case '+':
+      // case '-':{
+      // int restore_i = i + 1; // right
+      // i += 2;
+      // code_tree_ptr->left->left = malloc(sizeof(node));
+      // code_tree_ptr->left->left->type = PROGRAM;
+      // code_tree_ptr->left->left->left = malloc(sizeof(node));
+      // code_tree_ptr->left->left->right = malloc(sizeof(node));
+      // code_tree_ptr->left->left->back = code_tree_ptr->left;
+      // code_tree_ptr->left->right = malloc(sizeof(node));
+      // code_tree_ptr->left->right->type = END;
+      // code_tree_ptr->left->right->back = code_tree_ptr->left;
+
+      // int restore_i_minus_i = 1;
+      // token *left_token_argument = malloc(sizeof(token));
+      // memcpy(left_token_argument, &code_lex[restore_i],
+      //        sizeof(token));
+
+      // code_tree_ptr->left->type = (enum node_type) code_lex[restore_i - 1].type; // in eval, just check if the other arg is an end to determine if its a sign or not
+      // code_tree_ptr->left->back = code_tree_ptr;
+
+      // tree(code_tree_ptr->left->left, left_token_argument, 1);
+      // code_tree_ptr->right->back = code_tree_ptr;
+      // code_tree_ptr->right->left = malloc(sizeof(node));
+      // code_tree_ptr->right->right = malloc(sizeof(node));
+      // code_tree_ptr->right->type = PROGRAM;
+      // tree(code_tree_ptr->right, &code_lex[i], code_lex_index - i);
+      // return;}
+
     case 'T':;
       int restore_i = i; // left
       i--;
